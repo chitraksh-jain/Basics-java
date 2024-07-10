08f1a553a61cbffa4fa65c7d44f89ee78317cc90
chitraksh-jain <160010835+chitraksh-jain@users.noreply.github.com>
2024-07-10 19:25:03 +0530

    sum
    here the code helps you to sum of two float or int numbers.

---
+++ sum.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class sum{
+    public static void main(String args[]){
+        System.out.println("Enter Two Number:");
+        Scanner sc= new Scanner(System.in);
+        float num1= sc.nextFloat();
+        float num2 =sc.nextFloat();
+        float sum=num1 +num2;
+        System.out.print(sum);
+    }
+}
